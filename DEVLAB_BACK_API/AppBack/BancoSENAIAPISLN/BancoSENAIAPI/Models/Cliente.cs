b509813e65f1d6f94d9ada5b40b311c1876b8c94
iamcaiocampos <caio.c.carvalho6@aluno.senai.br>
2026-09-04 10:46:45 -0300

    criando parametros de cliente

---
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/Cliente.cs
@@ -0,0 +1,24 @@
+﻿namespace BancoSENAIAPI.Models
+{
+    public class Cliente
+    {
+        public int CodigoCliente { get; set; }
+
+        public int NomeCliente { get; set; }
+
+        public int CPF { get; set; }
+
+        public int NumeroDaAgencia { get; set; }
+
+        public int SaldoTotal { get; set; }
+
+        public int Sexo { get; set; }
+
+        public int Endereco { get; set; }
+
+        public int Cidade { get; set; }
+
+        public int Estado { get; set; }
+
+    }
+}
