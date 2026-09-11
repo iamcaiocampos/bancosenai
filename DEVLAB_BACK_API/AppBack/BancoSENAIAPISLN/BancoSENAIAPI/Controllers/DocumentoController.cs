710d554ec0a5bf5bd95729380fdfa33749ad5a77
iamcaiocampos <caio.c.carvalho6@aluno.senai.br>
2026-09-11 09:12:38 -0300

    metodo para salvar arquivo

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -32,6 +32,11 @@ namespace BancoSENAIAPI.Controllers
             string nomeOriginal = Path.GetFileName(arquivo.FileName);
             string novoNome = $"{codigoCliente}_{nomeOriginal}_{Guid.NewGuid()}{extensao}";
             string caminhoFinal = Path.Combine(pastaCliente, novoNome); 
+
+            using (var stream = new FileStream(caminhoFinal, FileMode.Create))
+            {
+                await arquivo.CopyToAsync(stream);
+            }
         }
     }
 }
